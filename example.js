547aae21c091cae1a906554f4dc18de161a66312
mudiageo <76590594+mudiageo@users.noreply.github.com>
2021-10-22 00:11:41 +0100

    Update example.js

--- example.js
+++ example.js
@@ -1,16 +1,14 @@
 const puppeteer = require('puppeteer');
 const cron = require('node-cron');
-//const proxy = require('./myproxy');
-// var proxy1 =  '--proxy-server='+  proxy.myProxy()
+const proxy = require('./myproxy');
 (async () => {
+  var myProxy = '--proxy-server='+proxy.myProxy;
   //var browser = await puppeteer.launch({ headless: false }); // default is true
-
-var browser = await puppeteer.launch();
-var page = await browser.newPage();
-//await page.authenticate();
+  var browser = await puppeteer.launch({args: [myProxy]});
+  var page = await browser.newPage();
+  await page.authenticate();
 await page.goto('https://ouo.io/FGGWvJ');
 await page.waitFor(5000);
-console.log('try');
 await page.click('#btn-main');
 await page.waitFor(5000);
 await page.click('#btn-main') ;
@@ -28,18 +26,14 @@ await browser.close();
 })();
 var task = cron.schedule('*/40 */2 * * * *', () => {
   (async () => {
-
+    var myProxy = '--proxy-server='+proxy.myProxy;
     //var browser = await puppeteer.launch({ headless: false }); // default is true
-//var proxy1 =  '--proxy-server='+  proxy.myProxy()
-(async () => {
-  //var browser = await puppeteer.launch({ headless: false }); // default is true
-
-//var browser = await puppeteer.launch({args: [proxy1]});
-    var browser = await puppeteer.launch();
+    var browser = await puppeteer.launch({args: [myProxy]});
     var page = await browser.newPage();
+    await page.authenticate();
+    
     await page.goto('https://ouo.io/FGGWvJ');
     await page.waitFor(5000);
-    console.log('try');
     await page.click('#btn-main');
     await page.waitFor(5000);
     await page.click('#btn-main') ;
@@ -51,7 +45,6 @@ var task = cron.schedule('*/40 */2 * * * *', () => {
     await page.click('#btn-main') ;
     await page.waitFor(5000);
     await page.click('#btn-main') ;
-    //await page.screenshot({ path: 'example.png' });
     await browser.close();
     
   })();
