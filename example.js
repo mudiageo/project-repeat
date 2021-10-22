6899b2efbe29481b34aa7724ec7b187bef662a84
mudiageo <76590594+mudiageo@users.noreply.github.com>
2021-10-22 11:40:05 +0100

    Update example.js

--- example.js
+++ example.js
@@ -1,12 +1,15 @@
 const puppeteer = require('puppeteer');
 const cron = require('node-cron');
-const proxy = require('./myproxy');
+//const proxy = require('./myproxy');
 (async () => {
-  var myProxy = '--proxy-server='+proxy.myProxy;
-  //var browser = await puppeteer.launch({ headless: false }); // default is true
-  var browser = await puppeteer.launch({args: [myProxy]});
+ // var myProxy = '--proxy-server='+proxy.myProxy;
+  var browser = await puppeteer.launch():
+                                       
+
+
+//  var browser = await puppeteer.launch({args: [myProxy]});
   var page = await browser.newPage();
-  await page.authenticate();
+  //await page.authenticate();
 await page.goto('https://ouo.io/FGGWvJ');
 await page.waitFor(5000);
 await page.click('#btn-main');
@@ -26,11 +29,11 @@ await browser.close();
 })();
 var task = cron.schedule('*/40 */2 * * * *', () => {
   (async () => {
-    var myProxy = '--proxy-server='+proxy.myProxy;
-    //var browser = await puppeteer.launch({ headless: false }); // default is true
-    var browser = await puppeteer.launch({args: [myProxy]});
+    //var myProxy = '--proxy-server='+proxy.myProxy;
+    var browser = await puppeteer.launch();// default is true
+   // var browser = await puppeteer.launch({args: [myProxy]});
     var page = await browser.newPage();
-    await page.authenticate();
+    //await page.authenticate();
     
     await page.goto('https://ouo.io/FGGWvJ');
     await page.waitFor(5000);
