a02c26e718efa56a7e751e35e22859a00845f7c8
mudiageo <76590594+mudiageo@users.noreply.github.com>
2021-10-27 14:45:35 +0100

    Update example.js

--- example.js
+++ example.js
@@ -2,38 +2,20 @@ const puppeteer = require('puppeteer');
 const cron = require('node-cron');
 //const proxy = require('./myproxy');
 (async () => {
- // var myProxy = '--proxy-server='+proxy.myProxy;
- 
- 
-
- 
-
-
-
-  var browser = await puppeteer.launch({
-                                       args: [
-
-        "--no-sandbox",
-        "--single-process",
-        "--no-zygote"
-    ]});
-
-
-//  var browser = await puppeteer.launch({args: [myProxy]});
+  //var myProxy = '--proxy-server='+proxy.myProxy;
+  var browser = await puppeteer.launch({ headless: false }); // default is true
+  //var browser = await puppeteer.launch();
   var page = await browser.newPage();
-  //await page.authenticate();
-await page.goto('https://ouo.io/FGGWvJ');
-await page.waitFor(5000);
+//  await page.authenticate();
+await page.goto('https://ouo.io/FGGWvJ').catch(error => console.log(error.message));
+await page.waitFor(5000).catch(error => console.log(error.message));
+
 await page.click('#btn-main');
-await page.waitFor(5000);
+await page.waitFor(5000).catch(error => console.log(error.message));
 await page.click('#btn-main') ;
-await page.waitFor(5000);
+await page.waitFor(5000).catch(error => console.log(error.message));
 await page.click('#btn-main') ;
-await page.waitFor(5000);
-    await page.click('#btn-main') ;
-    await page.waitFor(5000);
-    await page.click('#btn-main') ;
-    await page.waitFor(5000);
+await page.waitFor(5000).catch(error => console.log(error.message));
 await page.click('#btn-main') ;
 
 //await page.screenshot({ path: 'example.png' });
@@ -41,33 +23,22 @@ await browser.close();
 })();
 var task = cron.schedule('*/40 */2 * * * *', () => {
   (async () => {
-   var browser = await puppeteer.launch({
-                                       args: [
-
-        "--no-sandbox",
-        "--single-process",
-        "--no-zygote"
-    ]});
-    //var myProxy = '--proxy-server='+proxy.myProxy;
-    //var browser = await puppeteer.launch();// default is true
-   // var browser = await puppeteer.launch({args: [myProxy]});
+  //  var myProxy = '--proxy-server='+proxy.myProxy;
+    //var browser = await puppeteer.launch({ headless: false }); // default is true
+    var browser = await puppeteer.launch();
     var page = await browser.newPage();
-    //await page.authenticate();
+  //  await page.authenticate();
     
-    await page.goto('https://ouo.io/FGGWvJ');
-    await page.waitFor(5000);
+    await page.goto('https://ouo.io/FGGWvJ').catch(error => console.log(error.message));
+    await page.waitFor(5000).catch(error => console.log(error.message));
     await page.click('#btn-main');
-    await page.waitFor(5000);
+    await page.waitFor(5000).catch(error => console.log(error.message));
     await page.click('#btn-main') ;
-    await page.waitFor(5000);
+    await page.waitFor(5000).catch(error => console.log(error.message));
     await page.click('#btn-main') ;
-    await page.waitFor(5000);
+    await page.waitFor(5000).catch(error => console.log(error.message));
     await page.click('#btn-main') ;
-    await page.waitFor(5000);
-    await page.click('#btn-main') ;
-    await page.waitFor(5000);
-    await page.click('#btn-main') ;
-    await browser.close();
+    await browser.close().catch(error => console.log(error.message));
     
   })();
 });
