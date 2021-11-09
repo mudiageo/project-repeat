c01e515e186eaa60356cd08e347a1a4d28ab56e0
mudiageo <76590594+mudiageo@users.noreply.github.com>
2021-11-09 11:16:39 +0100

    Update example.js

--- example.js
+++ example.js
@@ -3,60 +3,61 @@ const cron = require('node-cron');
 //const proxy = require('./myproxy');
 (async () => {
   try{
-  //var myProxy = '--proxy-server='+proxy.myProxy;
-  var browser = await puppeteer.launch({ args: [
-        "--incognito",
-        "--no-sandbox",
-        "--single-process",
-        "--no-zygote"
-    ]}); // default is true
-  //var browser = await puppeteer.launch({
-  var page = await browser.newPage();
-//  await page.authenticate();
-await page.goto('https://ouo.io/jGWIxeU').catch(error => console.log(error.message));
-await page.waitFor(5000).catch(error => console.log(error.message));
-
-await page.click('#btn-main');
-await page.waitFor(5000).catch(error => console.log(error.message));
-await page.click('#btn-main') ;
-await page.waitFor(5000).catch(error => console.log(error.message));
-await page.click('#btn-main') ;
-await page.waitFor(5000).catch(error => console.log(error.message));
-await page.click('#btn-main') ;
-
-//await page.screenshot({ path: 'example.png' });
-await browser.close();
-    }catch(err){
-      console.log(err);
-      }
-})();
-var task = cron.schedule('*/40 */2 * * * *', () => {
-  (async () => {
-    try{
-  //  var myProxy = '--proxy-server='+proxy.myProxy;
+//  var myProxy = '--proxy-server='+proxy.myProxy;
     //var browser = await puppeteer.launch({ headless: false }); // default is true
-    var browser = await puppeteer.launch({args: [
-        "--incognito",
-        "--no-sandbox",
-        "--single-process",
-        "--no-zygote"
-    ]});
+    var browser = await puppeteer.launch();
     var page = await browser.newPage();
   //  await page.authenticate();
     
-    await page.goto('https://ouo.io/jGWIxeU').catch(error => console.log(error.message));
-    await page.waitFor(5000).catch(error => console.log(error.message));
+    await page.goto('https://ouo.io/FGGWvJ');
     await page.click('#btn-main');
-    await page.waitFor(5000).catch(error => console.log(error.message));
-    await page.click('#btn-main') ;
-    await page.waitFor(5000).catch(error => console.log(error.message));
-    await page.click('#btn-main') ;
-    await page.waitFor(5000).catch(error => console.log(error.message));
-    await page.click('#btn-main') ;
-    await browser.close().catch(error => console.log(error.message));
-      }catch(err){
-        console.log(err);
-        }
-    
+    await page.waitFor(5000);
+  
+  
+    await page.click('#btn-main')
+    await page.waitFor(5000);
+    await page.click('#btn-main')
+    await page.waitFor(5000);
+    await page.click('#btn-main')
+    await page.waitFor(5000);
+    await browser.close(); 
+  }    
+  catch(err) {
+    console.log(err);
+  }
+  
   })();
+var task = cron.schedule('*/20 * * * * *', () => {
+  (async () => {
+ 
+ try{
+      //  var myProxy = '--proxy-server='+proxy.myProxy;
+    //var browser = await puppeteer.launch({ headless: false }); // default is true
+    var browser = await puppeteer.launch();
+    var page = await browser.newPage();
+  //  await page.authenticate();
+  //await page.goto('file:///C:/xampp1/htdocs/npm/index.html');
+    
+    await page.goto('https://ouo.io/FGGWvJ');
+    await page.click('#btn-main');
+    await page.waitFor(5000);
+  
+  
+    await page.click('#button2')
+    await page.waitFor(5000);
+    await page.click('#button2')
+    await page.waitFor(5000);
+    await page.click('#button2')
+    await page.waitFor(5000);
+    await browser.close(); 
+      
+  
+}
+  catch(err) {
+    console.log(err);
+  }
+})();
+ 
+
 });
+
