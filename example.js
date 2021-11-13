c5a546234cfbb7eb5d7d136677d590476a005429
mudiageo <76590594+mudiageo@users.noreply.github.com>
2021-11-13 23:09:41 +0100

    Update example.js

--- example.js
+++ example.js
@@ -33,36 +33,11 @@ const puppeteer = require('puppeteer');
 
 
     await browser.close();
-                }catch(e){
+                }
+        catch(e){
                         console.log('38387'+e);
                         }
 })();
 
-var task = cron.schedule('*/20 * * * * *', () => {
- (async () => {
-         try{
-    const browser = await puppeteer.launch({
-        headless: true,
-    });
-    const page = await browser.newPage();
-
-    await page.goto('https://ouo.io/FGGWvJ',  {"waitUntil" : "networkidle0"});
-    await page.waitFor(5000);
-
-    await page.click('#btn-main');
-   await page.waitFor(5000);
-
-    await page.click('#btn-main');
-
-    await page.waitFor(5000);
-
-
-    await browser.close();
-                 }catch(e){
-                         
-console.log('CAtch'+e);
-                         }
-})();
-
-});
-
+//var task = cron.schedule('*/20 * * * * *', () => {
+ 
