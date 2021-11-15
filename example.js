2b7fc389e4581d28353fb72b714549b877cc919b
mudiageo <76590594+mudiageo@users.noreply.github.com>
2021-11-15 12:34:04 +0100

    Update example.js

--- example.js
+++ example.js
@@ -11,61 +11,28 @@
 //const puppeteer = require('puppeteer');
 const puppeteer = require("puppeteer-extra");
 
-// Add stealth plugin and use defaults (all tricks to hide puppeteer usage)
-const StealthPlugin = require("puppeteer-extra-plugin-stealth");
-puppeteer.use(StealthPlugin());
-
-// Add adblocker plugin to block all ads and trackers (saves bandwidth)
-const AdblockerPlugin = require("puppeteer-extra-plugin-adblocker");
-puppeteer.use(AdblockerPlugin({ blockTrackers: true }));
-
-
-
 (async () => {
         try{
     const browser = await puppeteer.launch({
-        headless: true,
-        args: [
-            "--no-sandbox",
-            "--disable-setuid-sandbox"
-        ],
+        headless: true
     });
-                console.log('gugohb1');
+
     const page = await browser.newPage();
-console.log('gugohb2');
-    await page.goto('https://ouo.io/FGGWvJ',  {"waitUntil" : "networkidle0"});
-  console.log('gugohb2');
+    await page.goto('https://ouo.io/FGGWvJ',  {"waitUntil" : "networkidle0", timeout: 0});
  //  const navigationPromise = await page.waitForNavigation({waitUntil: "domcontentloaded", timeout: 0});
   var context = await page;//.waitForSelector('#btn-main');//, {timeout: 0});
   await page.waitFor(5000);
                 console.log('gugohb3');
                    //await navigationPromise;
-console.log('gugohb4');
+
                 await context.click('#btn-main');
                 console.log('gugohb5');
  await page.waitFor(5000);
-                  await navigationPromise;
-console.log('gugohb1');
+                  //await navigationPromise;
+console.log('gugohb5');
 await context.click('#btn-main');
            await page.waitFor(5000);
-                  await navigationPromise;
-
-await context.click('#btn-main');
-               
-await page.waitFor(5000);
-                  await navigationPromise;
-
-await context.click('#btn-main');
-           await page.waitFor(5000);
-                  await navigationPromise;
-
-await context.click('#btn-main');
-               
-             await page.waitFor(5000);
-                  await navigationPromise;
-
-await context.click('#btn-main');
-  await navigationPromise;
+         
                
     await browser.close();
                 }
@@ -73,6 +40,8 @@ await context.click('#btn-main');
                         console.log('38387'+e);
                         }
 })();
+
+
 
 //var task = cron.schedule('*/20 * * * * *', () => {
  
