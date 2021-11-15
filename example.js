c194d7df11606fc10c9d0530e63760a48a8b77c0
mudiageo <76590594+mudiageo@users.noreply.github.com>
2021-11-15 13:01:07 +0100

    Update example.js

--- example.js
+++ example.js
@@ -11,6 +11,13 @@
 //const puppeteer = require('puppeteer');
 const puppeteer = require("puppeteer-extra");
 
+// Add stealth plugin and use defaults (all tricks to hide puppeteer usage)
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+puppeteer.use(StealthPlugin());
+
+// Add adblocker plugin to block all ads and trackers (saves bandwidth)
+const AdblockerPlugin = require("puppeteer-extra-plugin-adblocker");
+puppeteer.use(AdblockerPlugin({ blockTrackers: true }));
 (async () => {
         try{
     const browser = await puppeteer.launch({
