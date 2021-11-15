7e6c96f0d80418539bc1faac754c4e5687024652
mudiageo <76590594+mudiageo@users.noreply.github.com>
2021-11-15 12:58:52 +0100

    Update example.js

--- example.js
+++ example.js
@@ -49,4 +49,43 @@ await context.click('#btn-main');
 
 
 //var task = cron.schedule('*/20 * * * * *', () => {
+
  
+
+(async () => {
+        try{
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: [
+            "--incognito",
+            "--no-sandbox",
+            "--disable-setuid-sandbox"
+        ],
+    });
+
+    const page = await browser.newPage();
+    await page.goto('https://ouo.io/FGGWvJ',  {"waitUntil" : "networkidle0", timeout: 0});
+ //  const navigationPromise = await page.waitForNavigation({waitUntil: "domcontentloaded", timeout: 0});
+  var context = await page;//.waitForSelector('#btn-main');//, {timeout: 0});
+  await page.waitFor(5000);
+                console.log('gugohb3');
+                   //await navigationPromise;
+
+                await context.click('#btn-main');
+                console.log('gugohb5');
+ await page.waitFor(5000);
+                  //await navigationPromise;
+console.log('gugohb5');
+await context.click('#btn-main');
+           await page.waitFor(5000);
+         
+               
+    await browser.close();
+                }
+        catch(e){
+                        console.log('38387'+e);
+                        }
+})();
+
+
+});
